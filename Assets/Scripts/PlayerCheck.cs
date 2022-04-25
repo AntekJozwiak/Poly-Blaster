86b46f8a699d63b8e5afcb82b250d37dd0f9bc9d
AntekJozwiak <90831173+AntekJozwiak@users.noreply.github.com>
2022-04-25 21:30:53 +0100

    minor map upgrade

---
+++ Assets/Scripts/PlayerCheck.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fellaKilla : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Collided with player");
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+}
